873308daff3185e789e375c7c9c5458c67aaffed
davidemontagna <davidemontagna92@gmail.com>
2021-12-02 14:21:24 +0100

    Fine esercizio

--- myscript.js
+++ myscript.js
@@ -1,1 +1,5 @@
-let userInputName = prompt ("Inserisci il tuo nome:")
+let userInputName = prompt ("Inserisci il tuo nome:");
+let userInputSurname = prompt ("Inserisci il tuo cognome:");
+let userInputFavColor = prompt ("Inserisci il tuo colore preferito:");
+
+document.getElementById("pwd").innerHTML = userInputName + userInputSurname + userInputFavColor + "21";
